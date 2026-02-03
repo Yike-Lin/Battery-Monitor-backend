4d50c593fd997f6ef11e2268ea800fca007b4863
Yike-Lin <3100281535@qq.com>
2026-02-03 20:50:34 +0800

    单体电池详情实体BatteryRecord

---
+++ src/main/java/com/bms/backend/entity/BatteryRecord.java
@@ -0,0 +1,57 @@
+package com.bms.backend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "battery_record")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BatteryRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 关联到哪一块电池
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "battery_id",nullable = false)
+    private Battery battery;
+
+    // 循环编号
+    @Column(name = "cycle",nullable = false)
+    private Integer cycle;
+
+    // 分钟
+    @Column(name = "time_min",nullable = false)
+    private Double timeMin;
+
+    // 电压
+    @Column(name = "voltage",nullable = false)
+    private Double current;
+
+    // 温度
+    @Column(name = "temp")
+    private Double temp;
+
+    // 容量
+    @Column(name = "capacity")
+    private Double capacity;
+
+    // 来源文件名
+    @Column(name = "source_file")
+    private String sourceFile;
+
+    // 上传批次
+    @Column(name = "upload_batch")
+    private String uploadBatch;
+
+    // @Column(name = "created_at")
+    // private OffsetDateTime createdAt;
+
+
+}
