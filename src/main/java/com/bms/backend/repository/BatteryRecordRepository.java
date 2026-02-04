7e1bac76020df20c0674c0af08df1de0daa01119
Yike-Lin <3100281535@qq.com>
2026-02-04 19:25:03 +0800

    BatteryRecordRepository增加接口

--- src/main/java/com/bms/backend/repository/BatteryRecordRepository.java
+++ src/main/java/com/bms/backend/repository/BatteryRecordRepository.java
@@ -9,16 +9,25 @@ import java.util.List;
 
 public interface BatteryRecordRepository extends JpaRepository<BatteryRecord , Long> {
 
-    // 按电池查询所有记录
+    // 按电池ID 查询所有记录（分页）
     Page<BatteryRecord> findBatteryId(Long batteryId , Pageable pageable);
 
-    // 按电池 + cycle范围查询
+    // 按电池ID + cycle范围 查询(分页）
     Page<BatteryRecord> findByBatteryIdAndCycleBetween(Long batteryId,
                                                        Integer cycleStart,
                                                        Integer cycleEnd,
                                                        Pageable pageable);
 
-    // 按电池 + 单个cycle查询（画循环曲线用）
+    // 按电池ID + cycle >= 下限 查询（分页）
+    Page<BatteryRecord> findByBatteryIdAndCycleGreaterThanEqual(Long batteryId,
+                                                                Integer cycleStart,
+                                                                Pageable pageable);
+    // 按电池ID + cycle >= 上限 查询（分页）
+    Page<BatteryRecord> findByBatteryIdAndCycleLessThanEqual(Long batteryId,
+                                                             Integer cycleEnd,
+                                                             Pageable pageable);
+
+    // 按电池ID + 单个cycle查询（按timeMin升序，画曲线用）
     List<BatteryRecord> findByBatteryIdAndCycleOrderByTimeMinAsc(Long batteryId , Integer cycle);
 
 }
