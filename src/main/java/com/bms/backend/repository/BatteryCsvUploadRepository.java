b22ba75329b843df834d8596d5027e1cfcba1986
Yike-Lin <3100281535@qq.com>
2026-02-05 22:23:07 +0800

    BatteryCsvUploadRepository接口

---
+++ src/main/java/com/bms/backend/repository/BatteryCsvUploadRepository.java
@@ -0,0 +1,11 @@
+package com.bms.backend.repository;
+
+import com.bms.backend.entity.BatteryCsvUpload;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BatteryCsvUploadRepository extends JpaRepository<BatteryCsvUpload , Long> {
+
+    Optional<BatteryCsvUpload> findByUploadToken(String uploadToken);
+}
