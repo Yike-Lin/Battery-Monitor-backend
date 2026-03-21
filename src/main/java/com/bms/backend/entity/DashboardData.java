6d6a43db003d547361a5c41c88dfac808a06c38e
Yike_Lin <3100281535@qq.com>
2026-03-21 16:19:18 +0800

    绘图用的ID

--- src/main/java/com/bms/backend/entity/DashboardData.java
+++ src/main/java/com/bms/backend/entity/DashboardData.java
@@ -5,6 +5,11 @@ import lombok.Data;
 @Data
 public class DashboardData {
     private String time;      // 时间字符串 "HH:mm:ss"
+
+    // 用于绘图的电池ID
+    private String cellIdA;
+    private String cellIdB;
+
     private Double va = 0.0;  // Pack A 电压
     private Double ca = 0.0;  // Pack A 电流
     private Double vb = 0.0;  // Pack B 电压
