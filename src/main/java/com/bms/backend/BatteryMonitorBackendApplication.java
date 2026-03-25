1a9360b3769161bdcbe611a2a44982baa35934ca
Yike_Lin <3100281535@qq.com>
2026-03-25 12:01:30 +0800

    // 开启定时任务（拓扑快照聚合）

--- src/main/java/com/bms/backend/BatteryMonitorBackendApplication.java
+++ src/main/java/com/bms/backend/BatteryMonitorBackendApplication.java
@@ -3,9 +3,11 @@ package com.bms.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync			// 开启异步
+@EnableScheduling      // 开启定时任务（拓扑快照聚合）
 public class BatteryMonitorBackendApplication {
 
 	public static void main(String[] args) {
