ef41f0f183f5d4a16cf27a31eaeabdc6195315fd
Yike_Lin <3100281535@qq.com>
2026-03-25 21:35:18 +0800

    SohAnnotationRepository 用于操作 SOH 标注信息

---
+++ src/main/java/com/bms/backend/repository/SohAnnotationRepository.java
@@ -0,0 +1,8 @@
+package com.bms.backend.repository;
+
+import com.bms.backend.entity.SohAnnotation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SohAnnotationRepository extends JpaRepository<SohAnnotation, Long> {
+}
+
